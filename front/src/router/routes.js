4d8f86f9de1f3a53003b99aa4926fcb9a675c403
Anson Chung <>
2022-06-15 21:42:12 +0100

    Renamed path in router

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -11,7 +11,7 @@ const routes = [
         ]
     },
     {
-        path: "/template",
+        path: "/PortfolioProject",
         component: () => import("layouts/chatroomLayout.vue"),
         children: [
             { path: "", component: LoadPage("PageTemplate") },
